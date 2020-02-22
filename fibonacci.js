8d3875c17c10226370ace4705fa3c2e0bf3a3f5a
Massimiliano Puca <max.puca@gmail.com>
2020-02-22 19:24:16 +0100

    primo caricamento

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,18 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+let n = 30
+const serie = [0, 1]
+let somma
+function fibonacci(x) {
+    if (x === n) {
+        return
+    }
+    else {
+        somma = serie[x - 1] + serie[x - 2]
+        serie.push(somma)
+        return fibonacci(x + 1)
+    }
+}
+fibonacci(2)
+console.log(serie)
