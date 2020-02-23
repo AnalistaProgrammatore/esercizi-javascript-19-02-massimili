00504360368717f7b80e4329997bd8cfab34c7ff
Massimiliano Puca <max.puca@gmail.com>
2020-02-23 12:17:51 +0100

    secondo caricamento

--- mcd.js
+++ mcd.js
@@ -1,3 +1,30 @@
 /**
 * Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
 **/
+// calcolo del MCD col metodo iterativo
+let primoNumero = 96
+let secondoNumero = 57
+let resto
+
+function mcd(x, y) {
+    do {
+        resto = x % y
+        x = y
+        y = resto
+    } while (resto > 0)
+    return x
+}
+console.log("MCD trovato con metodo iterativo: " + mcd(primoNumero, secondoNumero))
+// calcolo del MCD col metodo ricorsivo
+function mcdRecursive(x, y) {
+    resto = x % y
+    if (resto === 0) {
+        return y
+    }
+    else {
+        x = y
+        y = resto
+        return mcdRecursive(x, y)
+    }
+}
+console.log("MCD trovato con metodo ricorsivo: " + mcdRecursive(primoNumero, secondoNumero))
